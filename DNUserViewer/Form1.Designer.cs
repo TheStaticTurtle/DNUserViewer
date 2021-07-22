e6b28aa7a1a2307f3082b933a4160ede4d88d5f5
Samuel <samuel.tugler@biotrial.com>
2021-07-22 15:40:35 +0200

    V1.0

---
+++ DNUserViewer/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿
+namespace DNUserViewer {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBoxUsername = new System.Windows.Forms.TextBox();
+			this.textBoxPassword = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBoxDomain = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBoxQueryUser = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.buttonQueryUsername = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label5 = new System.Windows.Forms.Label();
+			this.buttonExport = new System.Windows.Forms.Button();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.groupBox1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(14, 22);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(55, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Username";
+			// 
+			// textBoxUsername
+			// 
+			this.textBoxUsername.Location = new System.Drawing.Point(98, 19);
+			this.textBoxUsername.Name = "textBoxUsername";
+			this.textBoxUsername.Size = new System.Drawing.Size(179, 20);
+			this.textBoxUsername.TabIndex = 1;
+			// 
+			// textBoxPassword
+			// 
+			this.textBoxPassword.Location = new System.Drawing.Point(342, 19);
+			this.textBoxPassword.Name = "textBoxPassword";
+			this.textBoxPassword.Size = new System.Drawing.Size(181, 20);
+			this.textBoxPassword.TabIndex = 3;
+			this.textBoxPassword.UseSystemPasswordChar = true;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(283, 22);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(53, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Password";
+			// 
+			// textBoxDomain
+			// 
+			this.textBoxDomain.Location = new System.Drawing.Point(578, 19);
+			this.textBoxDomain.Name = "textBoxDomain";
+			this.textBoxDomain.Size = new System.Drawing.Size(210, 20);
+			this.textBoxDomain.TabIndex = 5;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(529, 22);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(43, 13);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Domain";
+			// 
+			// textBoxQueryUser
+			// 
+			this.textBoxQueryUser.Location = new System.Drawing.Point(98, 42);
+			this.textBoxQueryUser.Name = "textBoxQueryUser";
+			this.textBoxQueryUser.Size = new System.Drawing.Size(179, 20);
+			this.textBoxQueryUser.TabIndex = 7;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(14, 45);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(84, 13);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "Query username";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.AutoSize = true;
+			this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.groupBox1.Controls.Add(this.buttonQueryUsername);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.textBoxQueryUser);
+			this.groupBox1.Controls.Add(this.textBoxUsername);
+			this.groupBox1.Controls.Add(this.label4);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.textBoxDomain);
+			this.groupBox1.Controls.Add(this.textBoxPassword);
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.groupBox1.Location = new System.Drawing.Point(0, 0);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(800, 81);
+			this.groupBox1.TabIndex = 8;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Connection / Query infos";
+			// 
+			// buttonQueryUsername
+			// 
+			this.buttonQueryUsername.Location = new System.Drawing.Point(283, 42);
+			this.buttonQueryUsername.Name = "buttonQueryUsername";
+			this.buttonQueryUsername.Size = new System.Drawing.Size(108, 20);
+			this.buttonQueryUsername.TabIndex = 8;
+			this.buttonQueryUsername.Text = "Query user information";
+			this.buttonQueryUsername.UseVisualStyleBackColor = true;
+			this.buttonQueryUsername.Click += new System.EventHandler(this.buttonQueryUsername_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dataGridView1.Location = new System.Drawing.Point(0, 81);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.Size = new System.Drawing.Size(800, 369);
+			this.dataGridView1.TabIndex = 9;
+			// 
+			// panel1
+			// 
+			this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.panel1.Controls.Add(this.label5);
+			this.panel1.Controls.Add(this.buttonExport);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panel1.Location = new System.Drawing.Point(0, 422);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(800, 28);
+			this.panel1.TabIndex = 10;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.label5.Location = new System.Drawing.Point(0, 15);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(195, 13);
+			this.label5.TabIndex = 1;
+			this.label5.Text = "DNUserQuery V1.0 - By TheStaticTurtle";
+			// 
+			// buttonExport
+			// 
+			this.buttonExport.Dock = System.Windows.Forms.DockStyle.Right;
+			this.buttonExport.Enabled = false;
+			this.buttonExport.Location = new System.Drawing.Point(603, 0);
+			this.buttonExport.Name = "buttonExport";
+			this.buttonExport.Size = new System.Drawing.Size(197, 28);
+			this.buttonExport.TabIndex = 0;
+			this.buttonExport.Text = "Export JSON file for current query";
+			this.buttonExport.UseVisualStyleBackColor = true;
+			this.buttonExport.Click += new System.EventHandler(this.buttonExport_Click);
+			// 
+			// saveFileDialog1
+			// 
+			this.saveFileDialog1.DefaultExt = "json";
+			this.saveFileDialog1.FileName = "export.json";
+			this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.groupBox1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "DnUserQuery";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBoxUsername;
+		private System.Windows.Forms.TextBox textBoxPassword;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBoxDomain;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBoxQueryUser;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Button buttonQueryUsername;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button buttonExport;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.Label label5;
+	}
+}
+
---
+++ DNUserViewer/Form1.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices.Protocols;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DNUserViewer {
+	public partial class Form1 : Form {
+
+        Dictionary<string, string> exportDict = new Dictionary<string, string>();
+        public Form1() {
+			InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            this.textBoxDomain.Text = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().DomainName;
+            var name = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            var slashIndex = name.IndexOf("\\");
+            this.textBoxUsername.Text = slashIndex > -1 ? name.Substring(slashIndex + 1) : name.Substring(0, name.IndexOf("@"));
+            this.textBoxQueryUser.Text = slashIndex > -1 ? name.Substring(slashIndex + 1) : name.Substring(0, name.IndexOf("@"));
+        }
+
+        protected void ShowLDAPError(LdapException lexc) {
+            if (lexc.ToString().Contains("The LDAP server is unavailable")) {
+                MessageBox.Show("The LDAP server is unavailable", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            String error = lexc.ServerErrorMessage;
+            string pp = error.Substring(76, 4);
+            string ppp = pp.Trim();
+
+            if ("52e" == ppp) MessageBox.Show("Invalid Username or password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            if ("775​" == ppp) MessageBox.Show("User account locked.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            if ("525​" == ppp) MessageBox.Show("User not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            if ("530" == ppp) MessageBox.Show("Not permitted to logon at this time.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            if ("531" == ppp) MessageBox.Show("Not permitted to logon at this workstation.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            if ("532" == ppp) MessageBox.Show("Password expired.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            if ("533​" == ppp) MessageBox.Show("Account disabled.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+        public static string ByteArrayToString(byte[] ba) {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+
+        private void Query_User(String username, String password, String domain, String query) {
+
+            try {
+                if (username == "") {
+                    MessageBox.Show("Missing Username.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                } else if (password == "") {
+                    MessageBox.Show("Missing password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                } else {
+                    LdapConnection connection = new LdapConnection(domain);
+                    NetworkCredential credential = new NetworkCredential(username, password);
+                    connection.Credential = credential;
+                    connection.Bind();
+
+                    // after authenticate Loading user details to data table
+                    PrincipalContext ctx = new PrincipalContext(ContextType.Domain);
+                    UserPrincipal ld_user = UserPrincipal.FindByIdentity(ctx, query);
+                    DirectoryEntry up_User = (DirectoryEntry)ld_user.GetUnderlyingObject();
+                    DirectorySearcher deSearch = new DirectorySearcher(up_User);
+                    SearchResultCollection results = deSearch.FindAll();
+
+                    exportDict = new Dictionary<string, string>();
+                    buttonExport.Enabled = false;
+
+                    //int i = 0;
+                    if (results != null && results.Count > 0) {
+                        buttonExport.Enabled = true;
+                        DataTable dt = new DataTable();
+                        dt.Columns.Add("Key");
+                        dt.Columns.Add("Value");
+
+                        foreach (string rp in results[0].Properties.PropertyNames) {
+                            DataRow row = dt.NewRow();
+                            DataRow raw_row = dt.NewRow();
+                            bool add_raw = false;
+                            object r = results[0].Properties[rp][0];
+                            row["Key"] = rp.ToString();
+
+
+                            switch (r.GetType().ToString()) {
+                                case "System.DateTime":
+                                    row["Value"] = ((DateTime)(r)).ToLocalTime().ToString();
+                                    //Add gmt value
+                                    raw_row["Key"] = rp.ToString() + "_GMT";
+                                    raw_row["Value"] = r.ToString();
+                                    add_raw = true;
+                                    exportDict.Add(rp.ToString() + "_GMT", r.ToString());
+                                    break;
+                                case "System.Int64":
+                                    raw_row["Key"] = rp.ToString()+"_RAW";
+                                    raw_row["Value"] = r.ToString();
+                                    add_raw = true;
+                                    exportDict.Add(rp.ToString() + "_RAW", r.ToString());
+
+                                    if ((Int64)r > 110133216000000000) { // Assume LDAP timestamp is value is bigger than year 1950
+                                        DateTime datetime = new DateTime(1601, 01, 01, 0, 0, 0, DateTimeKind.Utc).AddTicks((Int64)r);
+                                        row["Value"] = datetime.ToLocalTime().ToString();
+                                        exportDict.Add(rp.ToString(), datetime.ToLocalTime().ToString());
+                                    } else {
+                                        row["Value"] = r.ToString();
+                                        exportDict.Add(rp.ToString(), r.ToString());
+                                    }
+                                    break;
+                                case "System.String":
+                                    row["Value"] = r.ToString();
+                                    exportDict.Add(rp.ToString(), r.ToString());
+                                    break;
+                                case "System.Byte[]":
+                                    row["Value"] = ByteArrayToString((System.Byte[])r);
+                                    exportDict.Add(rp.ToString(), ByteArrayToString((System.Byte[])r));
+                                    break;
+                                default:
+                                    Console.WriteLine(rp + " / " + r.GetType());
+                                    row["Value"] = r.ToString();
+                                    exportDict.Add(rp.ToString(), r.ToString());
+                                    break;
+                            }
+                            dt.Rows.Add(row);
+                            if(add_raw) dt.Rows.Add(raw_row);
+                        }
+                        dataGridView1.DataSource = dt;
+                        dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                        dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                        dataGridView1.Columns[0].ReadOnly = true;
+                        dataGridView1.Columns[1].ReadOnly = true;
+                        dataGridView1.ReadOnly = true;
+                    }
+
+                }
+
+            } catch (LdapException lexc) {
+                ShowLDAPError(lexc);
+            }/* catch (Exception) {
+                MessageBox.Show("Invalid Username or password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }*/
+        }
+
+        private void buttonQueryUsername_Click(object sender, EventArgs e) {
+            Query_User(textBoxUsername.Text, textBoxPassword.Text, textBoxDomain.Text, textBoxQueryUser.Text);
+        }
+
+		private void buttonExport_Click(object sender, EventArgs e) {
+            saveFileDialog1.FileName = "export_" + textBoxQueryUser.Text + ".json";
+            saveFileDialog1.ShowDialog();
+        }
+
+		private void saveFileDialog1_FileOk(object sender, CancelEventArgs e) {
+            File.WriteAllText(saveFileDialog1.FileName, Newtonsoft.Json.JsonConvert.SerializeObject(exportDict, Newtonsoft.Json.Formatting.Indented));
+        }
+	}
+}
